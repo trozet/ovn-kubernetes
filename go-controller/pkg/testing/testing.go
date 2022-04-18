c38ccda990cb1f6c1d7967549125f42e6fd41c5e
Tim Rozet <trozet@redhat.com>
2022-04-18 16:55:17 -0400

    Allow make check without root
    Skips tests that require sudo or special capabilities to execute. To
    run, use "make check NOROOT=TRUE"
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- go-controller/pkg/testing/testing.go
+++ go-controller/pkg/testing/testing.go
@@ -1,9 +1,17 @@
 package testing
 
 import (
+	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega/format"
 	kruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"os"
 )
+
+func OnSupportedPlatformsIt(description string, f interface{}) {
+	if os.Getenv("NOROOT") != "TRUE" {
+		ginkgo.It(description, f)
+	}
+}
 
 func init() {
 	// Gomega's default string diff behavior makes it impossible to figure
