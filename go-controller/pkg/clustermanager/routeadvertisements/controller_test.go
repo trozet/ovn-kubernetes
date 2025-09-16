246c76c086a0faf11ab93145373e6876ecc8beb2
Tim Rozet <trozet@nvidia.com>
2025-09-16 14:33:46 -0400

    Migrate fake network manager into the networkmanager package
    This is cleaner and needed to use the fake network manager inside of the
    networkmanager pkg.
    
    Signed-off-by: Tim Rozet <trozet@nvidia.com>

--- go-controller/pkg/clustermanager/routeadvertisements/controller_test.go
+++ go-controller/pkg/clustermanager/routeadvertisements/controller_test.go
@@ -33,7 +33,6 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	ovntest "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing"
-	nmtest "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/networkmanager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
@@ -1005,7 +1004,7 @@ func TestController_reconcile(t *testing.T) {
 			wf, err := factory.NewClusterManagerWatchFactory(fakeClientset)
 			g.Expect(err).ToNot(gomega.HaveOccurred())
 
-			nm, err := networkmanager.NewForCluster(&nmtest.FakeControllerManager{}, wf, fakeClientset, nil)
+			nm, err := networkmanager.NewForCluster(&networkmanager.FakeControllerManager{}, wf, fakeClientset, nil)
 			g.Expect(err).ToNot(gomega.HaveOccurred())
 
 			c := NewController(nm.Interface(), wf, fakeClientset)
